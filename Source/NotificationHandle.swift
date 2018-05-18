d85ad0080ccbada6991eeeb74dab9234f5d0b6d4
Michael Leber <michael.leber@markit.com>
2018-05-18 15:48:39 -0600

    update doc comments

--- Source/NotificationHandle.swift
+++ Source/NotificationHandle.swift
@@ -11,13 +11,13 @@ public class NotificationHandle {
     weak var center: NotificationCenter?
     
     
-    /// Creates a new NotificationHandle that will watch for a given notification
+    /// Creates a new `NotificationHandle` that will watch for a given notification
     ///
     /// - Parameters:
     ///   - name: The name of the notification
-    ///   - center: The notification center to watch from (Default is `NotificationCenter.default`)
+    ///   - center: The `NotificationCenter` to watch from (Default is `NotificationCenter.default`)
     ///   - filterObject: The object to filter on (Default is `nil`)
-    ///   - queue: The queue to send notifications on (Default is `OperationQueue.main`)
+    ///   - queue: The `OperationQueue` to send notifications on (Default is `OperationQueue.main`)
     ///   - closure: The call back closure
     init(_ name: Notification.Name, center: NotificationCenter = NotificationCenter.default, filterObject: Any? = nil, queue: OperationQueue = OperationQueue.main, closure: @escaping ((Notification) -> Void)) {
         self.token = center.addObserver(forName: name, object: filterObject, queue: queue, using: closure)
@@ -32,13 +32,17 @@ public class NotificationHandle {
 }
 
 
-// MARK: - Notification.Name extension
+// MARK: - Notification.Name extension for generating observable closures
 public extension Notification.Name {
     
     /// Signs up to watch the current Notification.Name - returned object should be retained by the client
-    ///
+    /// - Parameters:
+    ///   - center: The `NotificationCenter` to watch from (Default is `NotificationCenter.default`)
+    ///   - filterObject: The object to filter on (Default is `nil`)
+    ///   - queue: The `OperationQueue` to send notifications on (Default is `OperationQueue.main`)
+    ///   - closure: The call back closure
+    /// - Returns: A NotificationHandle that should be retaiend by the client
     func handle(_ center: NotificationCenter = NotificationCenter.default, filterObject: Any? = nil, queue: OperationQueue = OperationQueue.main, closure: @escaping ((Notification)-> Void)) -> NotificationHandle {
         return NotificationHandle(self, center: center, filterObject: filterObject, queue: queue, closure: closure)
     }
 }
-
